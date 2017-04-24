0dcd7bdc7be6899006b4f6e07dae275e86118c5e
cleex1004 <cleex1004@gmail.com>
2017-04-24 11:18:48 -0700

    set up auto layout class with methods

---
+++ CoreDataHotel/AutoLayout.h
@@ -0,0 +1,37 @@
+//
+//  AutoLayout.h
+//  CoreDataHotel
+//
+//  Created by Christina Lee on 4/24/17.
+//  Copyright © 2017 Christina Lee. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AutoLayout : NSObject
+
++(NSLayoutConstraint *)genericConstraintFrom:(UIView *)view
+                                      toView:(UIView *)superView
+                               withAttribute:(NSLayoutAttribute)attribute
+                               andMultiplier:(CGFloat)multiplier;
+
++(NSLayoutConstraint *)genericConstraintFrom:(UIView *)view
+                                      toView:(UIView *)superView
+                               withAttribute:(NSLayoutAttribute)attribute;
+
++(NSArray *)fullScreenConstraintsWithVFLForView:(UIView *)view;
+
++(NSLayoutConstraint *)equalHeightConstraintFrom:(UIView *)view
+                                          toView:(UIView *)otherView
+                                  withMultiplier:(CGFloat)multiplier;
+
++(NSLayoutConstraint *)leadingConstraintFrom:(UIView *)view
+                                      toView:(UIView *)otherView;
+
++(NSLayoutConstraint *)trailingConstraintFrom:(UIView *)view
+                                       toView:(UIView *)otherView;
+
+
+
+
+@end
