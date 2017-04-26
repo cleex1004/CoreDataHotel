e33a901713881654e2cd6b03fad7359c3647795d
cleex1004 <cleex1004@gmail.com>
2017-04-26 11:13:52 -0700

    break

--- CoreDataHotelTests/AutoLayoutTests.m
+++ CoreDataHotelTests/AutoLayoutTests.m
@@ -24,15 +24,58 @@
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     self.testController = [[UIViewController alloc]init];
+    self.testView1 = [[UIView alloc]init];
+    self.testView2 = [[UIView alloc]init];
+    [self.testController.view addSubview:self.testView1];
+    [self.testController.view addSubview:self.testView2];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    //tearing down properties
+    self.testController = nil;
+    self.testView1 = nil;
+    self.testView2 = nil;
+    
     [super tearDown];
 }
 
--(void)testViewControllerNotNil{
+//-(void)testViewControllerNotNil{
+//    XCTAssertNotNil(self.testController, @"The testController is nil!");
+//}
+
+-(void)testGenericConstraintFromToViewWithAttribute{
     XCTAssertNotNil(self.testController, @"The testController is nil!");
+    XCTAssertNotNil(self.testView1, @"self.testView1 is nil!");
+    XCTAssertNotNil(self.testView2, @"self.tesView2 is nile!");
+    
+    id constraint = [AutoLayout genericConstraintFrom:self.testView1 toView:self.testView2 withAttribute:NSLayoutAttributeTop];
+    
+    XCTAssert([constraint isKindOfClass:[NSLayoutConstraint class]], @"constraint is not an instance of NSLayoutConstraint");
+    
+    XCTAssertTrue([(NSLayoutConstraint *)constraint isActive], @"Constraint was not activated");
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
